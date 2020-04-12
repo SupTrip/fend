202fb6fbc156261b052d5450b0851fd48e914d2e
SupTrip <55024921+SupTrip@users.noreply.github.com>
2020-04-12 19:48:07 -0400

    Updated

--- projects/evaluate-news-nlp/src/client/js/formHandler.js
+++ projects/evaluate-news-nlp/src/client/js/formHandler.js
@@ -3,13 +3,20 @@ function handleSubmit(event) {
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
-    checkForName(formText)
+    Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
+    fetch('http://localhost:8081/api',
+    {
+        method: 'POST',
+       
+        body: JSON.stringify({formText}),
+        headers: {"Content-Type": "application/json"}
+    })
+
     .then(res => res.json())
     .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+        document.getElementById('results').innerHTML = `Input: ${reply.text} <br> Subjectivity: ${reply.subjectivity} <br> Polarity: ${reply.polarity} <br>`
     })
 }
 
