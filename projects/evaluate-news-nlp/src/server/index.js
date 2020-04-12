2466ba50f399bb3a9a23c32372b0be0675c4a60e
SupTrip <55024921+SupTrip@users.noreply.github.com>
2020-04-12 19:54:04 -0400

    Updated index.js

--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -1,23 +1,54 @@
+projectData = {};
+
+const dotenv = require('dotenv');
+dotenv.config();
+console.log(`Your aylienapi API key is ${process.env.API_KEY}`);
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+const aylien = require("aylien_textapi");
+var textapi = new aylien({
+  application_id: process.env.API_ID,
+  application_key: process.env.API_KEY
+});
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+
 
 const app = express()
 
 app.use(express.static('dist'))
+app.use(bodyParser.json());
+app.use(cors());
 
 console.log(__dirname)
 
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
+     //res.sendFile('dist/index.html')
     res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
 })
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+app.post('/api', async (req, res) => {
+    const { formText } = req.body;
+    console.log(formText);
+    
+    try {
+        console.log("Sending request");
+        textapi.sentiment({'text': formText }, 
+          function(error, response) {
+          if (error === null) {
+            console.log(response);
+            res.send(response);
+        }
+      });
+      
+    } catch(error) {
+      console.log(error);
+    }
+  })
