237dfb4d6c0279a3ea0cc470733b4d668067858c
SupTrip <55024921+SupTrip@users.noreply.github.com>
2020-04-12 19:42:39 -0400

    Update webpack-dev.js

--- projects/evaluate-news-nlp/webpack.dev.js
+++ projects/evaluate-news-nlp/webpack.dev.js
@@ -3,18 +3,29 @@ const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
+
 module.exports = {
     entry: './src/client/index.js',
+    output: {
+    libraryTarget: 'var',
+    library: 'Client'
+    },
+    
     mode: 'development',
     devtool: 'source-map',
     stats: 'verbose',
+   
     module: {
         rules: [
             {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
-            }
+            },
+            {
+    test: /\.scss$/,
+    use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+}
         ]
     },
     plugins: [
@@ -32,4 +43,5 @@ module.exports = {
             protectWebpackAssets: false
         })
     ]
+    
 }
